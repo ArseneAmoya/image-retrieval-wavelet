ad2a7898d3560701844244206d43bd33a135136a
Arsene <amoyaarsene@gmail.com>
2025-07-14 15:29:50 +0100

    solving decomposition level issue 2

--- roadmap/models/wresnet.py
+++ roadmap/models/wresnet.py
@@ -227,7 +227,8 @@ class WaveResNet(nn.Module):
         if self.att:
             if kwargs.get('attention_type', None) == "eca":
                 self.attention = Eca1D_layer(4)
-            self.attention = CBAM() #ChannelAttention(self.OUT_SIZE)
+            else:
+                self.attention = CBAM() #ChannelAttention(self.OUT_SIZE)
         else :
             self.attention = nn.Identity()
 
