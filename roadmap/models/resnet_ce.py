01e9a69a1d4fc3d8d353c0ab9ff1c71afd0ab0f2
Arsene <amoyaarsene@gmail.com>
2025-07-17 00:36:41 +0100

    relacing resnet50 by resnet 18

--- roadmap/models/resnet_ce.py
+++ roadmap/models/resnet_ce.py
@@ -1,19 +1,19 @@
 import torch
 import torch.nn as nn
 from torchvision import models
-from torchvision.models import ResNet50_Weights
+from torchvision.models import ResNet50_Weights, ResNet18_Weights
 from torchsummary import summary
 import torch.nn.functional as F
 
 class ResNetCE(nn.Module):
     def __init__(self, embed_dim: int, num_classes: int, pretrained: bool = False ):
         super().__init__()
-        self.backbone = models.resnet50(weights=ResNet50_Weights.DEFAULT)
+        self.backbone = models.resnet18(weights=ResNet18_Weights.DEFAULT)
         self.backbone.fc = nn.Identity()  # Remove the final fully connected layer
         #self.fc = nn.Linear(2048, embed_dim)
-        self.layer_norm = nn.BatchNorm1d(2048)
+        self.layer_norm = nn.BatchNorm1d(512)
         nn.LayerNorm(embed_dim)
-        self.classifier = nn.Linear(2048, num_classes)
+        self.classifier = nn.Linear(512, num_classes)
 
     def forward(self, x) -> torch.Tensor:
         x = self.backbone(x)
