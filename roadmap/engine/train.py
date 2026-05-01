90d112899b5890a783bc5b252dda93a8941ee8e3
Arsene <amoyaarsene@gmail.com>
2026-05-01 23:33:15 +0100

    Added

--- roadmap/engine/train.py
+++ roadmap/engine/train.py
@@ -100,7 +100,7 @@ def train(
                     exclude=["mean_average_precision_at_r", "mean_reciprocal_rank", "mean_average_precision",
                               "precision_at_1","recall_at_1","r_precision",'rpr', 'pr_rc', "recall_at_1000", "recall_at_100",
                               "recall_at_10", "recall_at_16", "recall_at_20", "recall_at_30", "recall_at_32", "recall_at_4", "recall_at_8",
-                                "recall_at_2", "recall_at_10"],
+                                "recall_at_2", "recall_at_10", "pr_rc_hashing"],
                     k=config.experience.evaluation.top_k,
                     distance_metric=config.experience.evaluation.distance_metric,
                     **dataset_dict,
